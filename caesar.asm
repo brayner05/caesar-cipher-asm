ef6718540bc5b325a6bf605fa77bfa80bbbd0fb1
Brayden <braydenrayner@outlook.com>
2024-01-24 22:33:39 -0400

    Fixed .gitignore (hopefully) and started implementing _encrypt.

--- caesar.asm
+++ caesar.asm
@@ -2,8 +2,8 @@
 offset: .byte 1
 max_length: .byte 254
 
-start_message: .string "Offset: "
-start_len: .byte 8
+msg: .string "Plaintext: "
+msg_len = . - msg
 
 plaintext: .zero 255
 ciphertext: .zero 255
@@ -14,8 +14,8 @@ ciphertext: .zero 255
 _start:
     movq $4, %rax   # sys_write
     movq $1, %rbx   # stdout
-    movq $start_message, %rcx
-    movq start_len, %rdx
+    movq $msg, %rcx
+    movq $msg_len, %rdx
     int $0x80
     
     movq $3, %rax   # sys_read
@@ -24,25 +24,30 @@ _start:
     movq max_length, %rdx
     int $0x80
 
+    call _encrypt
+
     movq $4, %rax   # sys_write
     movq $1, %rbx   # stdout
-    movq $plaintext, %rcx
-    movq max_length, %rdx
+    movq $ciphertext, %rcx
+    movq $5, %rdx
     int $0x80
 
 _exit:
     movq $1, %rax   # sys_exit
-    xorq %rbx, %rbx # Exit code 0
+    xorq %rbx, %rbx
     int $0x80
 
 _encrypt:
     movq $plaintext, %rax
-
-_a_strlen:
-    movq %rdi, %rax
-    movq $0, %rbx
+    jmp L1
+    ret
 L0:
+    movq $ciphertext, %rcx
+    addq %rax, %rcx
+    movb %bl, (%rcx)
     addq $1, %rax
-    cmpb '\0', %rax
-    jnz L0
+L1:
+    movb (%rax), %bl
+    testb %bl, %bl
+    jne L0
     ret
